d1d5c44c04a5e0f0eb3dc3e4817173b8d21a0576
huanghanzhilian <1319639755@qq.com>
2018-03-30 11:53:14 +0800

    20180326对老版本的ipad字幕兼容

--- src/videojs.ass.js
+++ src/videojs.ass.js
@@ -4,11 +4,8 @@
 
 (function (videojs, libjass) {
   'use strict';
-  var seasubtitle
-  var vjs_ass = function (options,seasubtitle) {
-    // console.log('++++获取字幕参数')
-    // console.log(options)
-    seasubtitle=seasubtitle
+
+  var vjs_ass = function (options,base) {
     var cur_id = 0,
       id_count = 0,
       overlay = document.createElement('div'),
@@ -112,71 +109,12 @@
       }
     })
 
-
-    function samuredDecoder(source) {
-      if (source) {
-        var lenth = source.replace(/(\d{1})(\S+)/, '$1')
-        var realSource = source.replace(/(\d{1})(\S+)/, '$2');
-        var decode1 = byteToString(URLSafeBase64.decode(realSource)).replace(new RegExp('(\\S+)(\\d{' + lenth + '})'), '$1')
-        return byteToString(URLSafeBase64.decode(decode1)).replace(new RegExp('(\\d{' + lenth + '})(\\S+)'), '$2');
-      }
-      return '';
-    };
-    function byteToString(arr) {  
-        if(typeof arr === 'string') {  
-            return arr;  
-        }  
-        var str = '',  
-        _arr = arr;  
-        for(var i = 0; i < _arr.length; i++) {  
-            var one = _arr[i].toString(2),  
-                v = one.match(/^1+?(?=0)/);  
-            if(v && one.length == 8) {  
-                var bytesLength = v[0].length;  
-                var store = _arr[i].toString(2).slice(7 - bytesLength);  
-                for(var st = 1; st < bytesLength; st++) {  
-                    store += _arr[st + i].toString(2).slice(2);  
-                }  
-                str += String.fromCharCode(parseInt(store, 2));  
-                i += bytesLength - 1;  
-            } else {  
-                str += String.fromCharCode(_arr[i]);  
-            }  
-        }  
-        return str;  
-    };
-
-
-
-
     rendererSettings = new libjass.renderers.RendererSettings();
-
-    var base = samuredDecoder(seasubtitle)
-      //var base=URLSafeBase64.decode(this.baseUrl)
-    libjass.ASS.fromString(base, libjass.Format.ASS).then((ass)=>{
-      //console.log('++++获取请求字幕结果11111111111111111111')
-      //console.dir(ass)
-      if (options.hasOwnProperty('enableSvg')) {
-        rendererSettings.enableSvg = options.enableSvg;
-      }
-      if (options.hasOwnProperty('fontMap')) {
-        rendererSettings.fontMap = new libjass.Map(options.fontMap);
-      } else if (options.hasOwnProperty('fontMapById')) {
-        rendererSettings.fontMap = libjass.renderers.RendererSettings
-          .makeFontMapFromStyleElement(document.getElementById(options.fontMapById));
-      }
-
-      addTrack(options.src, { label: options.label, srclang: options.srclang, switchImmediately: true });
-      renderers[cur_id] = new libjass.renderers.WebRenderer(ass, clocks[cur_id], overlay, rendererSettings);
-    })
-    //console.log(22222222222)
 
 
 
     // libjass.ASS.fromUrl(options.src, libjass.Format.ASS).then(
     //   function (ass) {
-    //     console.log('++++获取请求字幕结果')
-    //     console.log(ass)
     //     if (options.hasOwnProperty('enableSvg')) {
     //       rendererSettings.enableSvg = options.enableSvg;
     //     }
@@ -191,6 +129,62 @@
     //     renderers[cur_id] = new libjass.renderers.WebRenderer(ass, clocks[cur_id], overlay, rendererSettings);
     //   }
     // );
+    
+
+    function isIOS(){var u = navigator.userAgent;
+      var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
+      return isiOS;
+    }
+    
+    function getBrowser() {  
+      var ua = window.navigator.userAgent;  
+      var isIE = window.ActiveXObject != undefined && ua.indexOf("MSIE") != -1;  
+      var isFirefox = ua.indexOf("Firefox") != -1;  
+      var isOpera = window.opr != undefined;  
+      var isChrome = ua.indexOf("Chrome") && window.chrome;  
+      var isSafari = ua.indexOf("Safari") != -1 && ua.indexOf("Version") != -1;  
+      if (isIE) {  
+          return "IE";  
+      } else if (isFirefox) {  
+          return "Firefox";  
+      } else if (isOpera) {  
+          return "Opera";  
+      } else if (isChrome) {  
+          return "Chrome";  
+      } else if (isSafari) {  
+          return "Safari";  
+      } else {  
+          return "Unkown";  
+      }  
+    }  
+
+
+
+    libjass.ASS.fromString(base, libjass.Format.ASS).then(
+      function(ass){
+        if (options.hasOwnProperty('enableSvg')) {
+          rendererSettings.enableSvg = options.enableSvg;
+        }
+        if (options.hasOwnProperty('fontMap')) {
+          rendererSettings.fontMap = new libjass.Map(options.fontMap);
+        } else if (options.hasOwnProperty('fontMapById')) {
+          rendererSettings.fontMap = libjass.renderers.RendererSettings
+            .makeFontMapFromStyleElement(document.getElementById(options.fontMapById));
+        }
+        if(getBrowser()!='Safari'){
+          addTrack(options.src, { label: options.label, srclang: options.srclang, switchImmediately: true });
+        }
+        //console.log(getBrowser())
+        //addsppw()
+        
+        renderers[cur_id] = new libjass.renderers.WebRenderer(ass, clocks[cur_id], overlay, rendererSettings);
+      }
+    )
+
+
+
+
+
 
     function addTrack(url, opts) {
       var newTrack = player.addRemoteTextTrack({
@@ -248,7 +242,7 @@
       Experimental API use at your own risk!!
     */
     function loadNewSubtitle(url, label, srclang, switchImmediately) {
-      //console.log(url)
+
       var old_id = cur_id;
       if (switchImmediately) {
         renderers[cur_id]._removeAllSubs();
@@ -284,6 +278,6 @@
       loadNewSubtitle: loadNewSubtitle
     };
   };
-
-  videojs.plugin('ass', vjs_ass);
+  videojs.registerPlugin('ass', vjs_ass);
+  // videojs.plugin('ass', vjs_ass);
 }(window.videojs, window.libjass));
